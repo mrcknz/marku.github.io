90e36078e87d5756f568b63a79829f40170936ef
Marco Kunz <kunz.marco@gmail.com>
2022-07-24 22:24:39 +0200

    add fav icon

--- apps/wrthwhl/pages/_app.tsx
+++ apps/wrthwhl/pages/_app.tsx
@@ -9,7 +9,7 @@ export default function App(props: AppProps) {
   return (
     <>
       <Head>
-        <title>WRTHWHL</title>
+        <title>Marco Kunz</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
